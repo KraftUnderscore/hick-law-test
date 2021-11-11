14db6b37f48bb6f5fa8f3032271e4fc51a75ddee
Sebastian Pięta <spieta2@gmail.com>
2021-11-11 19:13:09 +0100

    refactor to array of elements

--- code.js
+++ code.js
@@ -1,42 +1,26 @@
 var times = [];
 
+var buttons = [];
+var answerBtns = [];
+var tasks = []; // TODO: add ending task with time displayed
+var taskOpt = [];
+
 window.addEventListener("DOMContentLoaded", (event) => {
-    console.log("LOADED");
-    introBtn = document.getElementById("introBtn");
-    introBtn.addEventListener("click", introPress);
-    fstBtn = document.getElementById("1st");
-    snBtn = document.getElementById("2nd");
-    thBtn = document.getElementById("3rd");
-    foBtn = document.getElementById("4th");
-    fiBtn = document.getElementById("5th");
+    buttons = document.getElementsByClassName("mainBtn");
+    tasks = Array.from(document.getElementsByClassName("hidden"));
+    taskOpt = document.getElementsByClassName("taskOpt");
+    answerBtns = document.getElementsByClassName("answer");
+    for (let i = 0; i < buttons.length; i++) {
+        const element = buttons[i];
+        element.addEventListener("click", (event => {
+            console.log(tasks);
+            switchTaskVisibility(tasks[i], tasks[i+1]);
+        }));
+    }
 
-    introDiv = document.getElementById("intro");
-    taskDiv1 = document.getElementById("task1");
-    taskDiv2 = document.getElementById("task2");
-    taskDiv3 = document.getElementById("task3");
-    taskDiv4 = document.getElementById("task4");
-    taskDiv5 = document.getElementById("task5");
+
+    tasks[0].className = "visible"; // display intro
 })
-
-var introBtn;
-var fstBtn;
-var snBtn;
-var thBtn;
-var foBtn;
-var fiBtn;
-
-var introDiv;
-var taskDiv1;
-var taskDiv2;
-var taskDiv3;
-var taskDiv4;
-var taskDiv5;
-
-var taskOpt1;
-var taskOpt2;
-var taskOpt3;
-var taskOpt4;
-var taskOpt5;
 
 var startTime = null;
 
@@ -50,15 +34,7 @@ function startTimer() {
     }
 }
 
-function switchVisibility(a, b) {
-    a.className = "hidden";
-    b.className = "visible";
-}
-
-function introPress() {
-    switchVisibility(introDiv, taskDiv1);
-}
-
-function Press1st() {
-
+function switchTaskVisibility(a, b) {
+    if(a) a.className = "hidden";
+    if(b) b.className = "visible";
 }
