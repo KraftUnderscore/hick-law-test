5066edf8f8bb7d79c75aaa342382c0588b9f57b9
Sebastian Pięta <spieta2@gmail.com>
2021-11-11 19:42:07 +0100

    functional version

--- code.js
+++ code.js
@@ -1,25 +1,37 @@
 var times = [];
 
-var buttons = [];
-var answerBtns = [];
-var tasks = []; // TODO: add ending task with time displayed
-var taskOpt = [];
+var continueBtns = [];
+var displayAnwsersBtns = [];
+var taskDivs = []; // TODO: add ending task with time displayed
+var answersBtns = [];
 
-window.addEventListener("DOMContentLoaded", (event) => {
-    buttons = document.getElementsByClassName("mainBtn");
-    tasks = Array.from(document.getElementsByClassName("hidden"));
-    taskOpt = document.getElementsByClassName("taskOpt");
-    answerBtns = document.getElementsByClassName("answer");
-    for (let i = 0; i < buttons.length; i++) {
-        const element = buttons[i];
-        element.addEventListener("click", (event => {
-            console.log(tasks);
-            switchTaskVisibility(tasks[i], tasks[i+1]);
+var resultsText;
+
+window.addEventListener("DOMContentLoaded", () => {
+    resultsText = document.getElementById("results");
+
+    continueBtns = document.getElementsByClassName("mainBtn");
+    taskDivs = Array.from(document.getElementsByClassName("hidden"));
+    answersBtns = Array.from(document.getElementsByClassName("taskOptOff"));
+    displayAnwsersBtns = document.getElementsByClassName("answer");
+
+    for (let i = 0; i < continueBtns.length; i++) {
+        const element = continueBtns[i];
+        element.addEventListener("click", (() => {
+            switchTaskVisibility(taskDivs[i], taskDivs[i+1]);
+            if(i!=0) startTimer();
         }));
     }
 
+    for (let i = 0; i < displayAnwsersBtns.length; i++) {
+        const element = displayAnwsersBtns[i];
+        element.addEventListener("click", (() => {
+            if(answersBtns[i]) answersBtns[i].className = "taskOpt"
+            startTimer();
+        }));
+    }
 
-    tasks[0].className = "visible"; // display intro
+    taskDivs[0].className = "visible"; // display intro
 })
 
 var startTime = null;
@@ -29,6 +41,7 @@ function startTimer() {
         var endTime = Date.now();
         times.push(endTime - startTime);
         startTime = null;
+        resultsText.innerHTML = times;
     } else {
         startTime = Date.now();
     }
